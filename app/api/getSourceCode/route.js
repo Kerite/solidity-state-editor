3536ee41b6a69dac22638b9af85d61a2dd8918ad
rockran <15927686537@139.com>
2024-09-06 17:40:32 +0800

    feat: dev code loop get source code

--- app/api/getSourceCode/route.js
+++ app/api/getSourceCode/route.js
@@ -1,6 +1,18 @@
 import { NextResponse } from "next/server";
 import axios from "axios";
 
+//Max rate limit reached, please use API Key for higher rate limit  【ps: dev key】
+const getLoopData = async (fetchUrl) => {
+  const res = await axios.get(fetchUrl);
+  if (
+    res.data.status === "0" &&
+    String(res.data.result).indexOf("Max rate limit reached") > -1
+  ) {
+    return await getData(fetchUrl);
+  }
+  return res;
+};
+
 export async function GET(request) {
   const url = new URL(request.url);
   const address = url.searchParams.get("address");
@@ -11,6 +23,6 @@ export async function GET(request) {
   const fetchUrl = `${ETHERSCAN_URL}?module=contract&action=getsourcecode&address=${address}&apikey=${APIKEY}`;
   console.log("fetchUrl", fetchUrl);
 
-  const { data } = await axios.get(fetchUrl);
+  const { data } = await getLoopData(fetchUrl);
   return NextResponse.json(data);
 }
